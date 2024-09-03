1b08d68716c922dc3a6b7c434d1d7f8e5d277384
Camila-CQ <101276419+Camila-CQ@users.noreply.github.com>
2024-09-03 14:53:07 -0500

    Update ApiService.dart

--- lib/ApiService.dart
+++ lib/ApiService.dart
@@ -1,11 +1,9 @@
-// lib/services/api_service.dart
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'Usuario.dart';
 
 class ApiService {
-  // Función para realizar la solicitud HTTP y obtener la lista de usuarios
   Future<List<Usuario>> fetchUsuarios() async {
     final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
 
@@ -17,12 +15,10 @@ class ApiService {
     }
   }
 
-  // Función para filtrar usuarios cuyo nombre de usuario tiene más de 6 caracteres
   List<Usuario> filtrarUsuariosPorNombreDeUsuario(List<Usuario> usuarios) {
     return usuarios.where((usuario) => usuario.nombreDeUsuario.length > 6).toList();
   }
 
-  // Función para contar usuarios cuyo correo pertenece al dominio `.biz`
   int contarUsuariosConDominioBiz(List<Usuario> usuarios) {
     return usuarios.where((usuario) => usuario.correo.endsWith('.biz')).length;
   }
